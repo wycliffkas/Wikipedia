e8e179f075ba5c175a4ed2add57b91e562c4cfdb
w4wycliff <w4wycliff@gmail.com>
2019-06-26 14:39:05 +0300

    intial commit

---
+++ app/src/main/java/com/r/wikipedia/activities/ArticleDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.r.wikipedia.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.r.wikipedia.R
+
+class ArticleDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_details)
+    }
+}
